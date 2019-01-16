94af51bc1b2856b2e260ba32df2c20f0e724d0e5
Liight <Liight@users.noreply.github.com>
2019-01-17 08:14:55 +1300

    Updated PlaceList Component View to FlatList to dynamically render a list with a scroll function
    Updated View to FlatList to dynamically render a list with a scroll function. Places array items have been changed to objects to hold a unique key and a value of the placeName. This is to be compatible with the FlatList component. Updated place added/deleted handlers accordingly.

--- App.js
+++ App.js
@@ -9,20 +9,24 @@ export default class App extends Component {
     places: []
   };
 
+  // Return an updated array to state.places with the current value of the placeName added as an object
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random().toString(), 
+          value: placeName
+        })
       };
     });
   };
 
-  // Return a list to state.places without the item with the index passed into the function
-  placeDeletedHandler = index => {
+  // Return an updated array to state.places without the object with the key passed into the function
+  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter((place) => {
+          return place.key !== key;
         })
       };
     });
