fab43c0400c9fcf88ff9834ec5b83a820e344599
Liight <Liight@users.noreply.github.com>
2019-01-17 09:50:16 +1300

    Added a Modal and a networked image
    Changed the image loaded with the places in the places listfrom a static local image to a networked image. Added PlaceDetail component to manage a modal. When a place list item is pressed it is now selected and not deleted. The modal buttons will close the modal and delete the modal respectively. Pressing the back button when the modal is up will close the modal.

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StyleSheet, View } from "react-native";
 
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
-import placeImage from './src/assets/image.jpg';
+// import placeImage from './src/assets/image.jpg';
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
 
 export default class App extends Component {
   state = {
-    places: []
+    places: [],
+    selectedPlace: null
   };
 
   // Return an updated array to state.places with the current value of the placeName added as an object
@@ -17,30 +19,56 @@ export default class App extends Component {
         places: prevState.places.concat({
           key: Math.random().toString(), 
           name: placeName,
-          image: placeImage
+          image: {
+            uri: "https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+          }
         })
       };
     });
   };
 
-  // Return an updated array to state.places without the object with the key passed into the function
-  placeDeletedHandler = key => {
+  // Return an updated array to state.places without the object with the key of state.selectedPlace
+  placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
         places: prevState.places.filter((place) => {
-          return place.key !== key;
-        })
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
       };
+    });
+  };
+
+  // Sets the state.selectedPlace to null forcing the modal to close
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    });
+  };
+
+  // Finds the object in state.places that has the matching key passed as args to this function
+  placeSelectedHandler = (key) => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
+        })
+      }
     });
   };
 
   render() {
     return (
       <View style={styles.container}>
+        <PlaceDetail 
+          selectedPlace={this.state.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+          />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
           places={this.state.places}
-          onItemDeleted={this.placeDeletedHandler}
+          onItemSelected={this.placeSelectedHandler}
         />
       </View>
     );
