9525b3de0be2593c57f52d68fc84946c1655dd6f
Liight <Liight@users.noreply.github.com>
2019-01-18 08:48:44 +1300

    Connected React-Redux to the application.
    Added mapStateToprops and mapDispatchToprops, connected them to the application and changed the default export to be the connect app (to the store). Stripped existing handler logic to reference actions.

--- App.js
+++ App.js
@@ -1,73 +1,46 @@
 import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
+import { connect } from 'react-redux';
 
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
 // import placeImage from './src/assets/image.jpg';
 import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions/index';
 
-export default class App extends Component {
-  state = {
-    places: [],
-    selectedPlace: null
-  };
+export class App extends Component {
 
   // Return an updated array to state.places with the current value of the placeName added as an object
   placeAddedHandler = placeName => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.concat({
-          key: Math.random().toString(), 
-          name: placeName,
-          image: {
-            uri: "https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-          }
-        })
-      };
-    });
+    this.props.onAddPlace(placeName);
   };
 
   // Return an updated array to state.places without the object with the key of state.selectedPlace
   placeDeletedHandler = () => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter((place) => {
-          return place.key !== prevState.selectedPlace.key;
-        }),
-        selectedPlace: null
-      };
-    });
+    this.props.onDeletePlace();
   };
 
   // Sets the state.selectedPlace to null forcing the modal to close
   modalClosedHandler = () => {
-    this.setState({
-      selectedPlace: null
-    });
+    this.props.onDeselectPlace();
   };
 
   // Finds the object in state.places that has the matching key passed as args to this function
   placeSelectedHandler = (key) => {
-    this.setState(prevState => {
-      return {
-        selectedPlace: prevState.places.find(place => {
-          return place.key === key;
-        })
-      }
-    });
+    this.props.onSelectPlace(key)
   };
 
   render() {
     return (
       <View style={styles.container}>
         <PlaceDetail 
-          selectedPlace={this.state.selectedPlace}
+          selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
           onModalClosed={this.modalClosedHandler}
           />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
-          places={this.state.places}
+          places={this.props.places}
           onItemSelected={this.placeSelectedHandler}
         />
       </View>
@@ -84,3 +57,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#F5FCFF"
   }
 });
+
+mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  };
+};
+
+mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: (name) => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: (key) => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
