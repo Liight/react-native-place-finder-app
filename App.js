1a0373a3f2201b7df21dcaf445623113a2b4e10b
Liight <Liight@users.noreply.github.com>
2019-01-24 10:45:25 +1300

    Add the redux store to the screens

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux'
 
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
 
 // Register Screens
-Navigation.registerComponent("react-native-place-finder-app.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("react-native-place-finder-app.SharePlaceScreen", () => SharePlaceScreen);
-Navigation.registerComponent("react-native-place-finder-app.FindPlaceScreen", () => FindPlaceScreen);
+Navigation.registerComponent("react-native-place-finder-app.AuthScreen", () => AuthScreen, store, Provider);
+Navigation.registerComponent("react-native-place-finder-app.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
+Navigation.registerComponent("react-native-place-finder-app.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
 
 // Start a App
 Navigation.startSingleScreenApp({
