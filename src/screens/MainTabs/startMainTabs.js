b8cec2a972df0a51350ccc688773851a8e59832e
Liight <Liight@users.noreply.github.com>
2019-01-24 12:26:42 +1300

    Add a side drawer when the menu icon is clicked on 2 screens

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -4,8 +4,10 @@ import Icon from 'react-native-vector-icons/Ionicons';
 const startTabs = () => {
     // Takes a set of promises and returns an array after all have resolved
     Promise.all([
+        // Icons return a promise 
         Icon.getImageSource("md-map", 30),
-        Icon.getImageSource("ios-share-alt", 30)
+        Icon.getImageSource("ios-share-alt", 30),
+        Icon.getImageSource("ios-menu", 30)
     ]).then(sources => {
         Navigation.startTabBasedApp({
             tabs: [
@@ -13,24 +15,43 @@ const startTabs = () => {
                     screen: "react-native-place-finder-app.FindPlaceScreen",
                     label: "Find Place",
                     title: "Find Place",
-                    icon: sources[0]
+                    icon: sources[0],
+                    navigatorButtons: {
+                        leftButtons: [
+                            {
+                                icon: sources[2],
+                                title: "Menu",
+                                id: "sideDrawerToggle"
+                            }
+                        ]
+                    }
                 },
                 {
                     screen: "react-native-place-finder-app.SharePlaceScreen",
                     label: "Share Place",
                     title: "Share Place",
-                    icon: sources[1]
+                    icon: sources[1],
+                    navigatorButtons: {
+                        leftButtons: [
+                            {
+                                icon: sources[2],
+                                title: "Menu",
+                                id: "sideDrawerToggle"
+                            }
+                        ]
+                    }
                 }
         
-            ]
+            ],
+            drawer: {
+                left: {
+                    screen: "react-native-place-finder-app.SideDrawerScreen"
+                }
+            }
         })
     })
-
-    // Icons return a promise 
-
-
-
-
 }
+
+    
 
 export default startTabs;
