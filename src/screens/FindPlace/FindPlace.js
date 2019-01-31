84f687ba49dad19ce211d9cae96a7917832c3b8a
Liight <Liight@users.noreply.github.com>
2019-02-01 12:16:24 +1300

    Animate findplace button

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import { connect } from 'react-redux';
 
 import PlaceList from '../../components/PlaceList/PlaceList';
@@ -8,6 +8,11 @@ class FindPlaceScreen extends Component {
     static navigatorStyle = {
         navBarButtonColor: "orange"
     }
+
+state = {
+    placesLoaded: false,
+    removeAnimation: new Animated.Value(1)
+}
 
     constructor(props) {
         super(props);
@@ -24,7 +29,13 @@ class FindPlaceScreen extends Component {
         }
       };
     
-
+      placesSearchHandler = () => {
+        Animated.timing(this.state.removeAnimation, {
+            toValue: 0,
+            duration: 500,
+            useNativeDriver: true
+        }).start();
+      }
 
     itemSelectedHandler = key => {
         const selectPlace = this.props.places.find(place => {
@@ -39,15 +50,60 @@ class FindPlaceScreen extends Component {
         })
     }
 
-    render(){
 
+
+    render(){
+        let content = (
+            <Animated.View style={{
+                opacity: this.state.removeAnimation,
+                transform: [{
+                    scale: this.state.removeAnimation.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [12, 1]
+                    })
+                }]
+            }}>
+            <TouchableOpacity onPress={this.placesSearchHandler}>
+                <View style={styles.searchButton}>
+                    <Text style={styles.searchButtonText}>Find Places</Text>
+                </View>
+            </TouchableOpacity>
+            </Animated.View>
+        );
+        if(this.state.placesLoaded){
+            content = (
+                <PlaceList 
+                    places={this.props.places} 
+                    onItemSelected={this.itemSelectedHandler}/>
+            );
+        }
         return (
-            <View>
-                <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler}/>
+            <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+                {content}
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    searchButton: {
+        borderColor: "orange",
+        borderWidth: 3,
+        borderRadius: 50,
+        padding: 20,
+        alignItems: "center",
+    },
+    searchButtonText: {
+        color: "orange",
+        fontWeight: "bold",
+        fontSize: 26
+    }
+});
 
 const mapStateToProps = state => {
     return {
